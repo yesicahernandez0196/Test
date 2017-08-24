72d20acc3d3d8c90f518e833abbaed7e12c7857a
yesicahernandez0196 <yesica.hernandez0196@gmail.com>
2017-08-24 17:19:24 -0600

    imprimir saludo

--- www/js/app.js
+++ www/js/app.js
@@ -6,8 +6,8 @@ var pi = 3.1416;
 var area = base * altura;
 var areaCirculo = pi * radio * radio;
 var areaRect = area / 2;
-var volEsfera = (4/3) * pi * radio * radio;
+var volEsfera = (4/3) * pi * radio * radio * radio;
 console.log("El area de mi rectangulo es igual: " + area); <!--"35"-->
 console.log("El area de mi circulo es = " + areaCirculo); <!--"19.635"-->
 console.log("El area de mi triangulo es = " + areaRect); <!--"17.5"-->
-console.log("El volumen de mi esfera es = " + volEsfera); <!--"26.18"-->
+console.log("El volumen de mi esfera es = " + volEsfera); <!--"65.45"-->
