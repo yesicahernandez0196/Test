822d62c7e311d3f43d6b6291c625c4a0d7c311aa
yesicahernandez0196 <yesica.hernandez0196@gmail.com>
2017-08-24 09:29:28 -0600

    hello git

---
+++ www/js/app.js
@@ -0,0 +1,13 @@
+var suma = 5 + 7;
+var base = 5; 
+var altura = 7;
+var radio = 2.5;
+var pi = 3.1416;
+var area = base * altura;
+var areaCirculo = pi * radio * radio;
+var areaRect = area / 2;
+var volEsfera = (4/3) * pi * radio * radio;
+console.log("El area de mi rectangulo es igual: " + area); <!--"35"-->
+console.log("El area de mi circulo es = " + areaCirculo); <!--"19.635"-->
+console.log("El area de mi triangulo es = " + areaRect); <!--"17.5"-->
+console.log("El volumen de mi esfera es = " + volEsfera); <!--"26.18"-->
