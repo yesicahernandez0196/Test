03925e54bb720c71acf2000757201109b7458865
yesica.hernandez0196 <yesica.hernandez0196@gmail.com>
2017-09-01 16:48:13 -0600

    tarea

--- www/js/platzom.js
+++ www/js/platzom.js
@@ -26,7 +26,24 @@ var length = str.length;
 if (length >= 10) {
   translation = str.slice(0, Math.round(length / 2)) + '-' + str.slice(Math.round(length / 2));
 }
+/*si la palabra original es un palindromo ninguna de las anteriores reglas funcionay se devuelve
+la palabra intercalando entre mayusculas y minusculas
+*/
 
+const reverse = (str)=> str.split('').reverse.join('')
+function minMay(str){
+    const l = str.length;
+    let translation= "";
+    let capitalize = true;
+    for(let i = 0; i <l; i++){
+        const char = str.charAt(i);
+        translation +=capitalize ? char.toUpperCase() : char.toLowerCase();
+
+    }
+}
+if(str = reverse(str)){
+    return minMay(str);
+}
 return translation;
 }
 
