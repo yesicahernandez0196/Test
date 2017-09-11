b3c2eca2a68a7a11689416e05f442b8aa9233dab
yesica.hernandez0196 <yesica.hernandez0196@gmail.com>
2017-09-11 09:59:01 -0600

    tareaRect

--- www/bonsaijs/www/js/app.js
+++ www/bonsaijs/www/js/app.js
@@ -1,4 +1,4 @@
-/*new Rect(10,10,100,100)
+new Rect(10,10,100,100)
 .addTo(stage)
 .attr('fillColor', 'cyan')
 
@@ -6,9 +6,9 @@
 .animate ('8s',{
     x:600,
     y:400
-})*/
+})
 
 
-new Circle (500, 500, 100)
+/*new Circle (500, 500, 100)
 .addTo(stage)
-.attr ('fillColor', 'red');
+.attr ('fillColor', 'red');*/
